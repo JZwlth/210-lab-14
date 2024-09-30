6bb16eb54aa6dbd38442fb6505fb1a0c79d2c190
JZwlth <wkjazy87@gmail.com>
2024-09-29 21:11:14 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
+// COMSC-210 | Lab 14 | Zhaoyi Zheng
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 class Color {
 private:
-    int red, green, blue;
+    int red;
+    int green;
+    int blue;
 
 public:
+    Color(int r = 0, int g = 0, int b = 0) : red(r), green(g), blue(b) {}
+
+    void setRed(int r);
+    void setGreen(int g);
+    void setBlue(int b);
+
+    int getRed() const;
+    int getGreen() const;
+    int getBlue() const;
+
+    void print() const;
+};
